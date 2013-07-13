ad3236f0f81b056a17bd1439850bd8dee13aabd7
Artimon <webmaster@pad-soft.de>
2013-07-13 19:03:44 +0200

    Adds password encryption/check class.

---
+++ leviathan/password.php
@@ -0,0 +1,25 @@
+<?php
+
+class Password {
+	/**
+	 * @param string $password
+	 * @param string $salt
+	 * @return string
+	 */
+	public static function crypt($password, $salt) {
+		$hash = md5($password);
+		$hash = crypt($hash, $salt);
+
+		return $hash;
+	}
+
+	/**
+	 * @param string $password
+	 * @param string $salt
+	 * @param string $hash
+	 * @return bool
+	 */
+	public static function valid($password, $salt, $hash) {
+		return (self::crypt($password, $salt) === $hash);
+	}
+}
